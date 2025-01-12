b1469d2f6ccf12c976e1d61a27d53f3307f5d8bb
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-12 15:15:48 +0100

    Update ModuleContext to use property instead of method for context

--- library/src/main/java/com/damolks/ouxy/module/ModuleContext.kt
+++ library/src/main/java/com/damolks/ouxy/module/ModuleContext.kt
@@ -1,12 +1,10 @@
 package com.damolks.ouxy.module
 
 import android.content.Context
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleCoroutineScope
 
 interface ModuleContext {
-    fun context(): Context 
+    val context: Context
     val lifecycleScope: LifecycleCoroutineScope
-    val lifecycle: Lifecycle
     val storage: StorageApi
 }
