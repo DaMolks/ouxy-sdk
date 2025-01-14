0d16d3e7551bcca62e5c791f1957f5cf28303c25
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-14 12:21:53 +0100

    feat: ajout de l'interface StorageApi

--- library/src/main/java/com/damolks/ouxy/module/StorageApi.kt
+++ library/src/main/java/com/damolks/ouxy/module/StorageApi.kt
@@ -1,8 +1,9 @@
 package com.damolks.ouxy.module
 
 interface StorageApi {
-    suspend fun writeText(path: String, content: String)
-    suspend fun readText(path: String): String
-    suspend fun exists(path: String): Boolean
-    suspend fun delete(path: String)
+    suspend fun readText(filename: String): String
+    suspend fun writeText(filename: String, content: String)
+    suspend fun delete(filename: String)
+    suspend fun exists(filename: String): Boolean
+    suspend fun list(): List<String>
 }
