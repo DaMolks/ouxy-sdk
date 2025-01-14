e027fdd0f851ee82b550c0676365212ff6e240c5
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-14 11:53:31 +0100

    fix: mise à jour de ModuleContext avec permissions

--- library/src/main/java/com/damolks/ouxy/module/ModuleContext.kt
+++ library/src/main/java/com/damolks/ouxy/module/ModuleContext.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import androidx.lifecycle.LifecycleCoroutineScope
 
 interface ModuleContext {
-    val context: Context
+    val applicationContext: Context
     val lifecycleScope: LifecycleCoroutineScope
     val storage: StorageApi
+    
+    fun hasPermission(permission: String): Boolean
+    suspend fun requestPermission(permission: String): Boolean
+    
     fun sendEvent(name: String, data: Map<String, Any>)
     fun registerEventHandler(name: String, handler: (Map<String, Any>) -> Unit)
 }
